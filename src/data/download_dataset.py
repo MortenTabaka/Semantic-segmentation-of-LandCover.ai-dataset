265d5a4a861b07f9d7c3d3782a5115be5cda124b
Marcin Tabaka <marcin.tabaka14@gmail.com>
2022-10-28 10:56:26 +0200

    fix: Correct path for downloading the dataset

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -6,10 +6,6 @@ import gdown
                 
 abs_path = os.path.abspath('')
 
-slash_idx = [idx for idx,ch in enumerate(abs_path) if ch=='/']
-
-abs_path = abs_path[:slash_idx[-2]]
-
 DATA_DIR = abs_path + '/data/raw/'
 
 output = DATA_DIR + 'landcover.zip'
