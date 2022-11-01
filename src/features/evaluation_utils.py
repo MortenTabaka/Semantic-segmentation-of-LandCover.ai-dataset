d42782b1fd83d4c63c9bdf9aa5a44214b3247c41
Marcin Tabaka <marcin.tabaka14@gmail.com>
2022-11-01 15:16:24 +0100

    Complete feature for displaying history plots

--- src/features/evaluation_utils.py
+++ src/features/evaluation_utils.py
@@ -5,6 +5,7 @@ import json
 from collections import defaultdict
 from json import dump
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class HistoryUtilities:
@@ -29,18 +30,40 @@ class History:
     def __init__(self):
         pass
 
-    @staticmethod
-    def display_single_history_plots(model_history: None | dict = None, history_path: None | str = None):
-
-        if history_path:
-            model_history = HistoryUtilities(history_path).load_model_history_from_file()
+    def display_history_plots(
+            self,
+            model_histories: None | list[dict] = None,
+            histories_filepaths: None | list[str] = None
+    ):
+        all_history = defaultdict()
+        if model_histories:
+            all_history = self.merge_multiple_histories(histories=model_histories)
+        elif histories_filepaths:
+            all_history = self.merge_multiple_histories(histories_filepaths=histories_filepaths)
 
         try:
-            for key in model_history.keys():
-                plt.plot(model_history[key])
-                plt.title(f"Training {key} (Epoch)")
-                plt.ylabel(f"{key}")
-                plt.xlabel("Epoch")
+            training_keys = []
+            # validation_keys = []
+
+            for key in all_history.keys():
+                if "val" not in key:
+                    print(key)
+                    training_keys.append(key)
+
+            for key in training_keys:
+                validation_key = "val_" + str(key)
+                number_of_epochs = len(all_history[key])
+
+                plt.figure(figsize=(12, 10), dpi=140)
+                plt.plot(np.arange(1, number_of_epochs + 1, step=1), all_history[key])
+                plt.plot(np.arange(1, number_of_epochs + 1, step=1), all_history[validation_key])
+                plt.title(f"Training {key} (epoch)", fontsize='x-large')
+                plt.ylabel(f"{key}".capitalize(), fontsize='large')
+                plt.xlabel("Epoch", fontsize='large')
+                plt.legend([f'{key}'.capitalize(), f'Validation {key}'], fontsize='large')
+                plt.xticks(np.arange(1, number_of_epochs + 1, step=1))
+                plt.grid()
+                # plt.savefig(data_save_dir + f'/{name}_all.jpg')
                 plt.show()
 
         except AttributeError as err:
@@ -53,19 +76,28 @@ class History:
             histories: None | list[dict] = None,
             histories_filepaths: None | list[str] = None
     ) -> defaultdict[list]:
+        """
+        Merges multiple histories into one.
+        Pass only one history's dictionaries or filepaths.
+        Args:
+            histories: list[dict]: list of histories dictionaries
+            histories_filepaths: list[str]: list of history filepaths
 
-        if histories_filepaths and not histories:
-            histories = []
+        Returns: Merged histories according to key.
+        """
+        added_histories = []
+
+        if histories:
+            for history in histories:
+                added_histories.append(history)
+        elif histories_filepaths:
             for filepath in histories_filepaths:
                 history = HistoryUtilities(filepath).load_model_history_from_file()
-                histories.append(history)
+                added_histories.append(history)
 
         merged = defaultdict(list)
-
-        if len(histories) > 1:
-            for single_history in histories:
-                for key, value in single_history.items():
-                    merged[key].append(value)
+        for single_history in added_histories:
+            for key, value in single_history.items():
+                merged[key] += value
 
         return merged
-
