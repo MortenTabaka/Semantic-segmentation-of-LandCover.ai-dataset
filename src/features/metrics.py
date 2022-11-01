89747b8bd4393fa98d7e83088bd10935f658404f
Marcin Tabaka <marcin.tabaka14@gmail.com>
2022-11-01 10:12:12 +0100

    fix: Correct y_true shape in metrics

--- src/features/metrics.py
+++ src/features/metrics.py
@@ -8,5 +8,6 @@ class CustomMeanIoU(tf.keras.metrics.MeanIoU):
         )
 
     def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = tf.math.argmax(y_true, axis=-1)
         y_pred = tf.math.argmax(y_pred, axis=-1)
         return super().update_state(y_true, y_pred, sample_weight)
