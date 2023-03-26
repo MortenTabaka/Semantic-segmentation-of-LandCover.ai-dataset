d87b308256fa188645ef38e39325cb22cb940d0a
Marcin Tabaka <marcin.tabaka14@gmail.com>
2023-03-26 18:49:18 +0200

    Feat: Download compressed dataset from HuggingFace https://huggingface.co/datasets/MortenTabaka/LandCover-Aerial-Imagery-for-semantic-segmentation/tree/main

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -1,14 +1,21 @@
-# https://github.com/wkentaro/gdown
-import os
+from os import path
+import requests
+from tqdm import tqdm
 
-import gdown
 
-abs_path = os.path.abspath("")
+abs_path = path.abspath("")
+output = abs_path + "/data/raw/"
 
-DATA_DIR = abs_path + "/data/raw/"
-
-output = DATA_DIR + "landcover.zip"
-
-if not os.path.isfile(output):
-    id = "1xILwUliGlWw5hmMt4E4GdDn5yV3ln8_G"
-    gdown.download(id=id, output=output, quiet=False)
+if not path.isfile(output):
+    url = "https://huggingface.co/datasets/MortenTabaka/LandCover-Aerial-Imagery-for-semantic-segmentation/resolve/main/landcover.zip"
+    response = requests.get(url, stream=True)
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    block_size = 1024
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(path.join(output, 'landcover.zip'), 'wb') as f:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            f.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR: Download incomplete")
