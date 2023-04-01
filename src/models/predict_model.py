95f87a6ccbc840f7f284bce4889b947f7c2e0474
Marcin Tabaka <marcin.tabaka14@gmail.com>
2023-04-01 22:54:44 +0200

    Refactor get_prediction_model_of_revision and get_model_revision_weights

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -8,6 +8,7 @@ from src.features.utils import (
     get_model_build_params_for_revision,
     get_revision_model_architecture,
 )
+from src.data.requests_downloader import UrlDownloader
 
 
 class Predictor:
@@ -23,8 +24,7 @@ class Predictor:
         )
 
     def get_multiple_batches_predictions(self, multiple_batch):
-        model = self.build_prediction_model_of_revision()
-        model.load_weights(self.get_model_revision_weights())
+        model = self.get_prediction_model_of_revision
         images_and_predictions = []
         for single_batch in multiple_batch:
             images = single_batch[0]
@@ -33,30 +33,35 @@ class Predictor:
         return images_and_predictions
 
     def get_single_batch_prediction(self, single_batch):
-        model = self.build_prediction_model_of_revision()
-        model.load_weights(self.get_model_revision_weights())
+        model = self.get_prediction_model_of_revision
         images = single_batch[0]
         y_pred = tf.argmax(model.predict(images), axis=-1)
         y_true = tf.argmax(single_batch[1], axis=-1)
 
         return images, y_true, y_pred
 
-    def build_prediction_model_of_revision(self):
+    @property
+    def get_prediction_model_of_revision(self):
         build_params = get_model_build_params_for_revision(self.model_key)
         model_name = get_revision_model_architecture(self.model_key)
-        return build_deeplabv3plus(model_name, build_params)
+        model = build_deeplabv3plus(model_name, build_params)
+        model.load_weights(self.get_model_revision_weights)
+        return model
 
+    @property
     def get_model_revision_weights(self):
         """
         Download and load revision model weights.
         Returns:
             model: tf.keras.Model
         """
-
-        weights = get_file(
-            fname=f"{self.model_key}.zip",
-            origin=self.url_with_zipped_weights,
-            extract=True,
-            cache_subdir="weights",
+        downloader = UrlDownloader()
+        weights_path = os.path.join(downloader.get_project_root(), "data/weights")
+        downloader.download_single_zip_file(
+            url=self.url_with_zipped_weights,
+            file_name=f"{self.model_key}.zip",
+            output_path=weights_path,
+            unzip=True
         )
-        return weights
+
+        return os.path.join(*[weights_path, self.model_key, "checkpoint"])
