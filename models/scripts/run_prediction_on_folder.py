b53c1a328ce2de559cf9fb5968a485711eee7d53
Marcin Tabaka <marcin.tabaka14@gmail.com>
2023-04-02 18:20:18 +0200

    Import and execute precessing by PredictionPipeline

--- models/scripts/run_prediction_on_folder.py
+++ models/scripts/run_prediction_on_folder.py
@@ -2,12 +2,13 @@ import typer
 from pathlib import Path
 
 from src.features.utils import get_absolute_path_to_project_location
+from src.pipelines.prediction_processor import PredictionPipeline
 
 
 def main(
     model_revision: str = typer.Option(
         default="deeplabv3plus_v5.10.2",
-        help="Choose model revision for running predictions",
+        help="Choose model revision used for predictions",
     ),
     model_revisions: Path = typer.Option(
         get_absolute_path_to_project_location("models/models_revisions.yaml"),
@@ -26,15 +27,18 @@ def main(
         show_default=False,
     ),
     output_folder: Path = typer.Option(
-        get_absolute_path_to_project_location("models/custom_data/output"),
-        help='Default: "models/custom_data/output". '
-        "Folder with predicted masks.",
+        default=get_absolute_path_to_project_location("models/custom_data/output"),
+        help='Default: "models/custom_data/output". Folder with predicted masks.',
         file_okay=False,
         dir_okay=True,
         show_default=False,
     ),
 ):
-    pass
+    PredictionPipeline(
+        model_revision=model_revision,
+        input_folder=input_folder,
+        output_folder=output_folder,
+    ).process()
 
 
 if __name__ == "__main__":
