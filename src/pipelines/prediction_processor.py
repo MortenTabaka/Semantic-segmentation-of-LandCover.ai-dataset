0ec31fb40d4824a56197c4c8ed2c8a2e0e2e636c
Marcin Tabaka <marcin.tabaka14@gmail.com>
2023-04-02 18:21:15 +0200

    Add preprocessing stage in process method of PredictionPipeline

--- src/pipelines/prediction_processor.py
+++ src/pipelines/prediction_processor.py
@@ -1,30 +1,40 @@
-from typing import List, Tuple
 from pathlib import Path
 
 import numpy as np
+from os.path import join as join_paths
 
 import tensorflow as tf
 
 from src.models.predict_model import Predictor
+from src.features.loss_functions import SemanticSegmentationLoss
+from src.data.image_preprocessing import ImagePreprocessor
 
 
 class PredictionPipeline:
-    def __init__(
-        self,
-        model_revision: str,
-        input_folder: Path,
-        output_folder: Path
-    ):
+    OPTIMIZER = tf.keras.optimizers.Adam()
+
+    def __init__(self, model_revision: str, input_folder: Path, output_folder: Path):
         self.predictor = Predictor(model_revision)
-        self.model = self.predictor.get_prediction_model_of_revision
-        self.image_input_specs = self.predictor.get_required_input_shape_of_an_image
+        self.model = self.predictor.get_prediction_model_of_revision.compile(
+            self.OPTIMIZER,
+            SemanticSegmentationLoss(
+                self.predictor.get_model_build_parameters[3]
+            ).soft_dice_loss,
+        )
+        self.input_image_shape = self.predictor.get_required_input_shape_of_an_image
         self.input_folder = input_folder
         self.output_folder = output_folder
 
-    def processing_pipeline(self):
-        pass
+    def process(self):
+        self.__preprocesses_images()
 
-    def load_images(self):
+    def __preprocesses_images(self):
+        ImagePreprocessor(self.input_folder).split_custom_images_before_prediction(
+            self.input_image_shape[0],
+            join_paths(self.input_folder, ".cache/tiles")
+        )
+
+    def __load_input_images(self):
         pass
 
     def make_predictions(self):
@@ -37,7 +47,7 @@ class PredictionPipeline:
         pass
 
     def make_prediction_for_single_tile(self, single_image: np.array):
-        if tf.shape(single_image) == ():
+        if tf.shape(single_image) == self.input_image_shape:
             return tf.argmax(self.model.predict(single_image), axis=-1)
         else:
             raise ValueError("Input image has incompatible shape.")
