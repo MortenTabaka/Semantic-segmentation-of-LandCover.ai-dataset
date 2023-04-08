1a0ab04e244aaececc4fbe6227a61a66ab7eab5d
Marcin Tabaka <marcin.tabaka14@gmail.com>
2023-04-08 16:00:37 +0200

    Fix: Correctly concat tiles according to its position from filename

--- src/data/image_postprocessing.py
+++ src/data/image_postprocessing.py
@@ -22,38 +22,51 @@ class ImagePostprocessor:
     def concatenate_images(self):
         img_filenames = sorted(os.listdir(self.input_path))
         base_names = self.__get_all_base_names_from_list_of_tiles(img_filenames)
-        separated_tiles_according_to_base_name = self.__get_tiles_according_its_base_name(
-            base_names, img_filenames
+        separated_tiles_according_to_base_name = (
+            self.__get_tiles_according_its_base_name(base_names, img_filenames)
         )
 
         for base_name, filenames in zip(
-                base_names,
-                separated_tiles_according_to_base_name
+            base_names, separated_tiles_according_to_base_name
         ):
-            img_tiles = []
+            filenames.sort()
 
-            for img_filename in filenames:
-                img_tile = cv2.imread(os.path.join(self.input_path, img_filename))
-                img_tiles.append(img_tile)
+            img_tile = cv2.imread(os.path.join(self.input_path, filenames[0]))
+            img_shape = img_tile.shape
 
-            img_shape = img_tiles[0].shape
-            num_cols = int(np.ceil(np.sqrt(len(img_tiles))))
-            num_rows = int(np.ceil(len(img_tiles) / num_cols))
+            (
+                vertical_multiplicative,
+                horizontal_multiplicative,
+            ) = self.__get_count_of_vertical_and_horizontal_tiles(filenames)
+
             img = np.zeros(
-                (num_rows * img_shape[0], num_cols * img_shape[1], img_shape[2]),
+                (
+                    vertical_multiplicative * img_shape[0],
+                    horizontal_multiplicative * img_shape[1],
+                    img_shape[2],
+                ),
                 dtype=np.uint8,
             )
 
+            num_of_tiles = len(filenames)
+
             k = 0
-            for i in range(num_rows):
-                for j in range(num_cols):
-                    if k >= len(img_tiles):
+            for v in range(vertical_multiplicative):
+                for h in range(horizontal_multiplicative):
+                    if k >= num_of_tiles:
                         break
+
+                    img_tile = cv2.imread(
+                        os.path.join(
+                            self.input_path,
+                            f"{base_name}_{self.__vertical}{v}_{self.__horizontal}{h}.jpg"
+                        )
+                    )
                     img[
-                        i * img_shape[0] : (i + 1) * img_shape[0],
-                        j * img_shape[1] : (j + 1) * img_shape[1],
+                        v * img_shape[0] : (v + 1) * img_shape[0],
+                        h * img_shape[1] : (h + 1) * img_shape[1],
                         :,
-                    ] = img_tiles[k]
+                    ] = img_tile
                     k += 1
             output_filename = os.path.join(self.output_path, base_name + ".jpg")
             cv2.imwrite(output_filename, img)
@@ -98,3 +111,18 @@ class ImagePostprocessor:
             split_tiles_according_to_base_name.append(tiles_of_base_image)
 
         return split_tiles_according_to_base_name
+
+    def __get_count_of_vertical_and_horizontal_tiles(self, tiles: List[str]):
+        vertical = []
+        horizontal = []
+
+        for tile in tiles:
+            num_vertical_tiles = int(
+                tile.split(self.__vertical)[1].split(f"_{self.__horizontal}")[0]
+            )
+            num_horizontal_tiles = int(tile.split(self.__horizontal)[1].split(f".")[0])
+
+            vertical.append(num_vertical_tiles)
+            horizontal.append(num_horizontal_tiles)
+
+        return max(vertical) + 1, max(horizontal) + 1
