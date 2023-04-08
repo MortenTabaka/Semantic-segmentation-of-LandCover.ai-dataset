66ed41b726b545a017e0cf6611676ecc8a3a616d
Marcin Tabaka <marcin.tabaka14@gmail.com>
2023-04-08 11:55:30 +0200

    Add docstring

--- models/scripts/run_prediction_on_folder.py
+++ models/scripts/run_prediction_on_folder.py
@@ -26,7 +26,24 @@ def main(
         dir_okay=True,
         show_default=False,
     ),
-):
+) -> None:
+    """
+    Run the prediction pipeline.
+
+    Parameters:
+    -----------
+    model_revision : str, optional
+        Choose model revision for predictions. Default is "deeplabv3plus_v5.10.2".
+    input_folder : Path, optional
+        Folder with images for which predictions should be made. Default is "models/custom_data/input".
+    output_folder : Path, optional
+        Folder with predicted masks. Default is "models/custom_data/output".
+
+    Returns:
+    --------
+    None
+    """
+
     PredictionPipeline(
         model_revision=model_revision,
         input_folder=input_folder,
