14c1db03065dd354ac16e9c111adc1cdf0203ffd
Marcin Tabaka <marcin.tabaka14@gmail.com>
2023-04-09 12:06:03 +0200

    Add citation for all but one loss functions

--- src/features/loss_functions.py
+++ src/features/loss_functions.py
@@ -19,6 +19,28 @@ class SemanticSegmentationLoss(object):
 
     Shruti Jadon, SemSegLoss: A python package of loss functions for semantic segmentation, 2021.
     https://github.com/shruti-jadon/Semantic-Segmentation-Loss-Functions
+
+    @inproceedings{jadon2020survey,
+        title={A survey of loss functions for semantic segmentation},
+        author={Jadon, Shruti},
+        booktitle={2020 IEEE Conference on Computational Intelligence in Bioinformatics and Computational Biology (CIBCB)},
+        pages={1--7},
+        year={2020},
+        organization={IEEE}
+    }
+    @article{JADON2021100078,
+        title = {SemSegLoss: A python package of loss functions for semantic segmentation},
+        journal = {Software Impacts},
+        volume = {9},
+        pages = {100078},
+        year = {2021},
+        issn = {2665-9638},
+        doi = {https://doi.org/10.1016/j.simpa.2021.100078},
+        url = {https://www.sciencedirect.com/science/article/pii/S2665963821000269},
+        author = {Shruti Jadon},
+        keywords = {Deep Learning, Image segmentation, Medical imaging, Loss functions},
+        abstract = {Image Segmentation has been an active field of research as it has a wide range of applications, ranging from automated disease detection to self-driving cars. In recent years, various research papers proposed different loss functions used in case of biased data, sparse segmentation, and unbalanced dataset. In this paper, we introduce SemSegLoss, a python package consisting of some of the well-known loss functions widely used for image segmentation. It is developed with the intent to help researchers in the development of novel loss functions and perform an extensive set of experiments on model architectures for various applications. The ease-of-use and flexibility of the presented package have allowed reducing the development time and increased evaluation strategies of machine learning models for semantic segmentation. Furthermore, different applications that use image segmentation can use SemSegLoss because of the generality of its functions. This wide range of applications will lead to the development and growth of AI across all industries.}
+    }
     """
 
     def __init__(
